254c2d4a828e007292d4bf48cebdbbf7fa6a38f4
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2022-11-10 00:35:31 -0600

    Site name update

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Digital Disciple',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Jonathan James',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
