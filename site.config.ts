c15c9961c579035a2de1135ce1e4cea5a614ed80
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2022-11-10 00:40:45 -0600

    Url

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Digital Disciple',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'https://nextjs-notion-digital-garden.vercel.app',
   author: 'Jonathan James',
 
   // open graph metadata (optional)
