0cf21130635f21b7919988777e3dfd6554095fad
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2023-01-09 15:34:31 -0500

    adds ChatGPT colution

--- pages/[pageId].tsx
+++ pages/[pageId].tsx
@@ -1,91 +1,37 @@
-import * as React from 'react'
-import { GetStaticProps } from 'next'
+import { useRouter } from "next/router";
+import { getPage } from "../lib/notion";
+import { BlockMapType } from "notion-client";
+import { Page } from "../types";
 
-import { NotionPage } from '@/components/NotionPage'
-import { domain, isDev } from '@/lib/config'
-import { getSiteMap } from '@/lib/get-site-map'
-import { resolveNotionPage } from '@/lib/resolve-notion-page'
-import { PageProps, Params } from '@/lib/types'
+const PageTemplate = ({ page }: { page: Page }) => {
+  const router = useRouter();
+  const pageId = router.query.pageId as string;
 
-export const getStaticProps: GetStaticProps<PageProps, Params> = async (
-  context
-) => {
-  const rawPageId = context.params.pageId as string
-
-  try {
-    const props = await resolveNotionPage(domain, rawPageId)
-
-    return { props, revalidate: 10 }
-  } catch (err) {
-    console.error('page error', domain, rawPageId, err)
-
-    // we don't want to publish the error version of this page, so
-    // let next.js know explicitly that incremental SSG failed
-    throw err
+  if (!page) {
+    return <div>Loading...</div>;
   }
-}
-
-export async function getStaticPaths() {
-  if (isDev) {
-    return {
-      paths: [],
-      fallback: true
-    }
-  }
-
-  const siteMap = await getSiteMap()
-
-  const staticPaths = {
-    paths: Object.keys(siteMap.canonicalPageMap).map((pageId) => ({
-      params: {
-        pageId
-      }
-    })),
-    // paths: [],
-    fallback: true
-  }
-
-  console.log(staticPaths.paths)
-  return staticPaths
-}
-
-export default function NotionDomainDynamicPage(props) {
-  return <NotionPage {...props} />
-}
-
-//chatgpt starts here
-interface MyPageProps {
-  page: {
-    id: string
-    children: {
-      title: string
-      rich_text: {
-        type: string
-        text: string
-        code: string
-      }[]
-    }[]
-  }
-}
-
-const Page: React.FC<MyPageProps> = ({ page }) => {
-  const children: React.ReactElement[] = page.children[0].rich_text.map((block, i) => {
-    if (block.type === 'text') {
-      return <p key={i}>{block.text}</p>
-    } else if (block.type === 'code') {
-      return (
-        <pre key={i}>
-          <code>{block.code}</code>
-        </pre>
-      )
-    }
-  })
 
   return (
-    <NotionPage key={page.id}>
-      <h1>{page.children[0].title}</h1>
-      {children}
-    </NotionPage>
-  )
-}
+    <div>
+      <h1>{page.properties.title[0][0]}</h1>
+      <BlockMapType blocks={page.properties.blocks} />
+    </div>
+  );
+};
 
+export const getStaticPaths = async () => {
+  // ...
+};
+
+export const getStaticProps = async ({ params }) => {
+  const pageId = params?.pageId as string;
+  const page = await getPage(pageId);
+
+  return {
+    props: {
+      page,
+    },
+  };
+};
+
+export default PageTemplate;
