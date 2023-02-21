64ae052161102842bbecc9d35ef0d2e8b8665fe3
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2023-02-21 17:52:18 -0500

    reverting notion page id

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '919d48cd92104d7b90d9bee32e872c05',
+  rootNotionPageId: '6fca5f70e69848c2a1d67308b06e421a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
